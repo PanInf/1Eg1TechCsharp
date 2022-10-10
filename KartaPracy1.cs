64f0850417854c7bc77da7f88dbca90fc666105b
PanInf <77333045+PanInf@users.noreply.github.com>
2022-10-10 09:58:18 +0200

    Dodanie pliku KartaPracy1.cs

---
+++ KartaPracy1.cs
@@ -0,0 +1,48 @@
+// Zad 1
+/*
+int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+Console.WriteLine(a*a + b*b);
+*/
+
+// Zad 2
+/*
+int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+Console.WriteLine(Math.Pow((a + b),2));
+*/
+
+// Zad 3
+/*
+int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+Console.WriteLine(Math.Pow((a - b), 3));
+*/
+
+// Zad 4
+/*
+int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+int c = int.Parse(Console.ReadLine());
+Console.WriteLine(a * b * c);
+*/
+
+// Zad 5
+/*
+int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+Console.WriteLine(2*(a+b)/5);
+*/
+
+// Zad 6
+/*
+double brutto = double.Parse(Console.ReadLine());
+Console.WriteLine(brutto / 1.23);
+*/
+
+// Zad 7
+/*
+int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+Console.WriteLine(a % b);
+*/
