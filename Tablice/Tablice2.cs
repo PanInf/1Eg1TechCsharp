88745272358df1d2dd9f043df3368b54eba2f108
PanInf <77333045+PanInf@users.noreply.github.com>
2022-12-12 09:48:59 +0100

    Create Tablice2.cs

---
+++ Tablice/Tablice2.cs
@@ -0,0 +1,20 @@
+// ROZGRZEWKA
+// Stwórz tablicę 100 losowych liczb 2-cyfrowych
+// podaj sumę liczb parzystych i ilość liczb nieparzystych
+
+
+
+// TABLICE 2-WYMIAROWE
+// inicjowanie tablicy
+
+
+// Zadeklaruj macierz a'la sudoku
+
+
+// Stwórz losową macierz 4x4 zawerającą dowolne cyfry
+
+
+//wypełnić macierz 3x5 liczbami dwucyfrowymi korzystając z GetLength()
+
+
+}
